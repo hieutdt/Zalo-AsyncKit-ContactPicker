9419e303ca6b86dbbf2389239e35ece0c319f1c9
hieutran <trandinhtonhieu2@gmail.com>
2020-04-24 17:50:12 +0700

    Update ComponentKit

--- AsyncKit ContactPicker/Presentation/Fragments/PickerTable/CK/CKPickerTableCellComponent.mm
+++ AsyncKit ContactPicker/Presentation/Fragments/PickerTable/CK/CKPickerTableCellComponent.mm
@@ -9,8 +9,15 @@
 #import "CKPickerTableCellComponent.h"
 #import "AppConsts.h"
 #import "ImageHelper.h"
+#import "StringHelper.h"
+
+#import <ComponentKit/CKComponentSubclass.h>
 
 @interface CKPickerTableCellComponent ()
+
+@property (nonatomic, strong) CKImageComponent *checkerImageComponent;
+@property (nonatomic, strong) CKImageComponent *avatarImageComponent;
+@property (nonatomic, strong) PickerViewModel *viewModel;
 
 @end
 
@@ -19,6 +26,11 @@
 + (instancetype)newWithPickerViewModel:(PickerViewModel *)viewModel
                                context:(ImageCache *)context {
     CKComponentScope scope(self);
+    if (viewModel.isChosen) {
+        scope.replaceState(scope, @YES);
+    } else {
+        scope.replaceState(scope, @NO);
+    }
     
     UIImage *avatarImage = [[UIImage alloc] init];
     switch (viewModel.gradientColorCode) {
@@ -36,9 +48,55 @@
             break;
     }
     
-    avatarImage = [ImageHelper makeRoundedImage:avatarImage radius:30.f];
+    UIImage *checkImage = [[UIImage alloc] init];
+    if (viewModel.isChosen) {
+        checkImage = [UIImage imageNamed:@"checked"];
+    } else {
+        checkImage = [UIImage imageNamed:@"uncheck"];
+    }
     
-    CKPickerTableCellComponent *c = [super newWithComponent:
+//    avatarImage = [ImageHelper makeRoundedImage:avatarImage radius:30.f];
+    
+    CKImageComponent *checkerImageComponent =
+    [CKImageComponent
+       newWithImage:checkImage
+       attributes:{}
+       size:{
+        .height = 30, .width = 30
+    }];
+    
+    CKLabelComponent *shortNameLabel =
+    [CKLabelComponent
+       newWithLabelAttributes:{
+        .string = [StringHelper getShortName:viewModel.name],
+        .color = [UIColor whiteColor],
+        .font = [UIFont boldSystemFontOfSize:20],
+        .alignment = NSTextAlignmentCenter
+    }
+       viewAttributes:{
+        {@selector(setBackgroundColor:), [UIColor clearColor]},
+        {@selector(setContentMode:), UIViewContentModeScaleToFill},
+        {@selector(setUserInteractionEnabled:), NO}
+    }
+     size:{}];
+    
+    CKCenterLayoutComponent *centerTextComponent =
+    [CKCenterLayoutComponent
+     newWithCenteringOptions:CKCenterLayoutComponentCenteringXY
+     sizingOptions:CKCenterLayoutComponentSizingOptionDefault
+     child:shortNameLabel size:{
+        .width = 60, .height = 60
+    }];
+    
+    CKImageComponent *avatarComponent =
+    [CKImageComponent
+    newWithImage:avatarImage
+    attributes:{}
+    size:{
+        .height = 60, .width = 60
+    }];
+    
+    CKFlexboxComponent *flexBoxComponent =
     [CKFlexboxComponent
      newWithView:{
         [UIView class],
@@ -53,18 +111,10 @@
         .spacing = 15
     }
      children:{
-        { [CKImageComponent
-           newWithImage:[UIImage imageNamed:@"uncheck"]
-           attributes:{}
-           size:{
-            .height = 25, .width = 25
-        }] },
-        { [CKImageComponent
-           newWithImage:avatarImage
-           attributes:{}
-           size:{
-            .height = 60, .width = 60
-        }] },
+        {checkerImageComponent},
+        {[CKOverlayLayoutComponent
+          newWithComponent:avatarComponent
+          overlay:centerTextComponent]},
         { [CKLabelComponent
             newWithLabelAttributes:{
                 .string = viewModel.name,
@@ -76,13 +126,34 @@
                 {@selector(setUserInteractionEnabled:), NO}
             }
             size:{}] }
-    }]];
+    }];
+    
+    CKPickerTableCellComponent *c = [super newWithComponent:
+                                     [CKInsetComponent newWithInsets:UIEdgeInsetsMake(10, 15, 15, 10)
+                                                           component:flexBoxComponent]];
+    
+    if (c) {
+        c.checkerImageComponent = checkerImageComponent;
+        c.avatarImageComponent = avatarComponent;
+        c.viewModel = viewModel;
+    }
     
     return c;
 }
 
++ (id)initialState {
+    return @NO;
+}
+
 - (void)didTap {
-    NSLog(@"Did tappppppppppp");
+    [self updateState:^(id *oldState) {
+        self.viewModel.isChosen = !self.viewModel.isChosen;
+        if (self.viewModel.isChosen) {
+            return @YES;
+        } else {
+            return @NO;
+        }
+    } mode:CKUpdateModeAsynchronous];
 }
 
 @end
